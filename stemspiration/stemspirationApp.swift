97c90d1e8cb88ddbf44b3fe3871f72a04771dd07
shrikap <shrikapunuganti@gmail.com>
2023-07-12 15:02:05 -0700

    Initial Commit

---
+++ stemspiration/stemspirationApp.swift
@@ -0,0 +1,17 @@
+//
+//  stemspirationApp.swift
+//  stemspiration
+//
+//  Created by Scholar on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct stemspirationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
